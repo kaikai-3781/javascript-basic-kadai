6ac284760c8c525fde6c6166a55b7d0ebcb3d6a7
kaikai-3781 <163139786+kaikai-3781@users.noreply.github.com>
2024-05-10 19:10:42 +0900

    Add files via upload

---
+++ kadai _007/calc.js
@@ -0,0 +1,18 @@
+let num =  15;
+
+// 3と5の倍数//
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+
+//3の倍数 //
+} else if (num % 3 === 0) {
+    console.log('3の倍数です');
+
+//5の倍数 //
+} else if (num % 5 === 0) {
+    console.log('5の倍数です');
+
+//それ以外の場合
+} else {
+    console.log(num);
+}
